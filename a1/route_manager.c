4819d55944e3614ce3952a5057bfb18839391ec9
Felipe Rivera Vera <rivera@morgan.seng.uvic.ca>
2023-01-26 14:25:32 -0800

    Updated a1 files for bshkwarok.

---
+++ a1/route_manager.c
@@ -0,0 +1,29 @@
+/** @file route_manager.c
+ *  @brief A pipes & filters program that uses conditionals, loops, and string processing tools in C to process airline routes.
+ *  @author Felipe R.
+ *  @author Hausi M.
+ *  @author Jose O.
+ *  @author Saasha J.
+ *  @author Victoria L.
+ *  @author STUDENT_NAME
+ *
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * Function: main
+ * --------------
+ * @brief The main function and entry point of the program.
+ *
+ * @param argc The number of arguments passed to the program.
+ * @param argv The list of arguments passed to the program.
+ * @return int 0: No errors; 1: Errors produced.
+ *
+ */
+int main(int argc, char *argv[])
+{
+    // TODO: your code.
+    exit(0);
+}
