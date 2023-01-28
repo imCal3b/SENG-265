07a4ba83178bce5e3e4be735282f92ca31f7377a
Brayden Shkwarok <brady82@live.ca>
2023-01-27 21:40:47 -0800

    started read_arguments() function

--- a1/route_manager.c
+++ a1/route_manager.c
@@ -5,12 +5,14 @@
  *  @author Jose O.
  *  @author Saasha J.
  *  @author Victoria L.
- *  @author Brayden Shkwarok V00866278
+ *  @author STUDENT_NAME
  *
  */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+void read_arguments();
 
 /**
  * Function: main
@@ -25,5 +27,27 @@
 int main(int argc, char *argv[])
 {
     // TODO: your code.
+    read_arguments(argc,argv);
+
     exit(0);
 }
+
+/*
+FUNCTION: looks through the arguments specified when running the program
+Parameters: int argc - the number of arguments
+            char **argv - a pointer to the array of pointers pointing to the
+            specified arguments
+return: NA
+PreConditions: must be at least one argument specified at program call
+*/
+void read_arguments(int argc, char **argv)
+{
+    char *data_field[13], *argument[13];
+
+    if (argc > 0) {
+        for (int i=0;i<argc;i++) {
+            printf("%s \n",*(argv + i));
+        }
+    }
+}
+
