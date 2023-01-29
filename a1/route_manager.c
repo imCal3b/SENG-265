f5f5525d8e443cdd45508be372ac1fc02de30321
Brayden Shkwarok <brady82@live.ca>
2023-01-29 10:40:53 -0800

    changed argument read/store from arrays to struct

--- a1/route_manager.c
+++ a1/route_manager.c
@@ -14,6 +14,13 @@
 
 void read_arguments();
 
+struct input {
+    char data_field[20];
+    char argument[40];
+
+    struct input *next;
+};
+
 /**
  * Function: main
  * --------------
@@ -27,7 +34,31 @@ void read_arguments();
 int main(int argc, char *argv[])
 {
     // TODO: your code.
-    read_arguments(argc,argv);
+    struct input head;
+    struct input *cur_ptr;
+    cur_ptr = &head;
+    printf("initializing input storage...\n\n");
+    for (int i=0;i<argc-1;++i) {
+        struct input next_input;       // create new pointer
+        cur_ptr->next = &next_input;    // assign new pointers address to cur.next
+        cur_ptr = cur_ptr->next;        // set cur to point to next
+        cur_ptr->next = NULL;
+    }
+
+    printf("head addr: %p\n",&head);
+    printf("h_df addr: %p\n",&(head.data_field));
+    printf("h_ag addr: %p\n",&(head.argument));
+    printf("h_ag addr: %p\n\n",head.next);
+
+    read_arguments(argc,argv,1,&head);
+
+    cur_ptr = &head;
+    int count = 0;
+    while (cur_ptr->next != NULL || count < 5) {
+        printf("field: %s arg: %s\n",cur_ptr->data_field,cur_ptr->argument);
+        ++count;
+        cur_ptr = cur_ptr->next;
+    }
 
     exit(0);
 }
@@ -40,14 +71,26 @@ Parameters: int argc - the number of arguments
 return: NA
 PreConditions: must be at least one argument specified at program call
 */
-void read_arguments(int argc, char **argv)
+void read_arguments(int argc, char **argv, int count, struct input *cur_ptr) 
 {
-    char *data_field[13], *argument[13];
+    if (argc == 1) {
+        printf("no arguments given\n");
+        return;
 
-    if (argc > 0) {
-        for (int i=0;i<argc;i++) {
-            printf("%s \n",*(argv + i));
-        }
+    } else if (count == argc || count > 13) {
+        printf("\nall arguments read: %d arguments\n\n",--count);
+        return;
+
+    } else {
+        printf("reading fields\n\n");
+        sscanf(*(argv+count),"--%[A-Z]=%[^\t\n]",cur_ptr->data_field,cur_ptr->argument);
+
+        printf("curr addr: %p\n",cur_ptr);
+        printf("c_df addr: %p\n",&(cur_ptr->data_field));
+        printf("c_ag addr: %p\n",&(cur_ptr->argument));
+        printf("c_ag addr: %p\n",cur_ptr->next);
+
+        cur_ptr = cur_ptr->next;
+        return read_arguments(argc,argv,++count,cur_ptr);
     }
 }
-
