857a7435b6f293c9fe835b01661f44c87366cc5c
Brayden Shkwarok <brady82@live.ca>
2023-03-01 13:06:48 -0800

    changes to get_inputs() and create_arg_dataframes(). Start question()
    get_inputs() fixed issue where program could not find file because the file path was incorrect. prefix './a/' added to file names.

--- a2/route_manager.py
+++ a2/route_manager.py
@@ -9,6 +9,7 @@ Sample input: --AIRLINES="airlines.yaml" --AIRPORTS="airports.yaml" --ROUTES="ro
 """
 import pandas as pd
 import yaml
+import json
 import sys
 import os
 
@@ -16,31 +17,116 @@ import os
 Function:   Read and parse the input arguments from program call
             and store data values in the list parameters passed
             to the function.
-Inputs:     arg_type: list - list to store the argument types.
-            arg: list - list of the specified arguments.
-Return: NA
+Inputs:     NA.
+Return:     dict - A dictionary containing the input arguments.
 """
-def get_inputs(arg_type: list, arg: list) -> int:
+def get_inputs() -> dict:
+    arg_type: str
+    arg: str
+
     if len(sys.argv) == 1:
         print("No Arguments Given...")
         return 0
     
+    inputs: dict = dict({})
     for i in range(1, len(sys.argv)):
         temp1, temp2 = sys.argv[i].split("=")
-        arg_type.append(temp1.split("--")[1])
-        arg.append(temp2)
+        arg_type = temp1.split("--")[1]
 
-    return len(sys.argv)-1
+        if ".yaml" in temp2: 
+            arg = "./a2/" + temp2
+        else:
+            arg = temp2
 
-    
+        inputs.update({arg_type:arg})
+
+    return inputs
+
+"""
+Function:   Take the input arguments and open the specified yaml
+            files as pandas DataFrames.
+Inputs:     inputs: dict - Dictionary of the input arguments.
+Return:     list - List containing the created DataFrames.
+"""
+def create_arg_dataframes(inputs: dict) -> list:
+    df_list: list = []
+
+    for key in inputs:
+
+        if ".yaml" in inputs[key]:
+            with open(inputs[key], "r") as arg_file:
+
+                if key == "AIRLINES":
+                    airlines = yaml.safe_load(arg_file)
+                    airlines_df: pd.DataFrame = pd.json_normalize(airlines["airlines"])
+
+                    df_list.append(airlines_df)
+
+                elif key == "AIRPORTS":
+                    airports = yaml.safe_load(arg_file)
+                    airports_df: pd.DataFrame = pd.json_normalize(airports["airports"])
+
+                    df_list.append(airports_df)
+                
+                elif key == "ROUTES":
+                    routes = yaml.safe_load(arg_file)
+                    routes_df: pd.DataFrame = pd.json_normalize(routes["routes"])
+
+                    df_list.append(routes)
+
+                else:
+                    continue
+
+                arg_file.close()
+
+    return df_list
+
+"""
+Function:   a
+"""
+def question(inputs: dict) -> None:
+     parameters: dict
+     key: str = "QUESTION"
+     if key in inputs:
+         print(f"Question: {inputs[key]}")
+
+         if inputs[key] == "q1":
+            # specify the important data fields for producing
+            # the output to the question.
+            parameters = {"field1" : "add", "field2" : "add2"}
+
+         if inputs[key] == "q2":
+            # specify the important data fields for producing
+            # the output to the question.
+            parameters = {"field1" : "add", "field2" : "add2"}
+
+         if inputs[key] == "q3":
+            # specify the important data fields for producing
+            # the output to the question.
+            parameters = {"field1" : "add", "field2" : "add2"}
+
+         if inputs[key] == "q4":
+            # specify the important data fields for producing
+            # the output to the question.
+            parameters = {"field1" : "add", "field2" : "add2"}
+
+         if inputs[key] == "q5":
+            # specify the important data fields for producing
+            # the output to the question.
+            parameters = {"field1" : "add", "field2" : "add2"}
+        
 
 def main():
-    arg_type: list = []
-    arg: list = []
-    argc: int = get_inputs(arg_type, arg)
+    inputs_dict: dict = get_inputs()
 
-    for i in range(argc):
-        print(f"{arg_type[i]} : {arg[i]}")
+    # print inputs
+    for key in inputs_dict:
+        print(f"{key} : {inputs_dict[key]}")
+    print()
+
+    df_list: list = create_arg_dataframes(inputs_dict)
+
+    question(inputs_dict)
 
 
 if __name__ == '__main__':
