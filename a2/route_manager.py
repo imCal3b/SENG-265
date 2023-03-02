e5eb348634500d5afd55f4d35f4ba887f48ff3e7
Brayden Shkwarok <brady82@live.ca>
2023-03-02 08:28:13 -0800

    added comments specifying data fields of the DataFrames

--- a2/route_manager.py
+++ a2/route_manager.py
@@ -9,7 +9,6 @@ Sample input: --AIRLINES="airlines.yaml" --AIRPORTS="airports.yaml" --ROUTES="ro
 """
 import pandas as pd
 import yaml
-import json
 import sys
 import os
 
@@ -57,22 +56,25 @@ def create_arg_dataframes(inputs: dict) -> list:
             with open(inputs[key], "r") as arg_file:
 
                 if key == "AIRLINES":
-                    airlines = yaml.safe_load(arg_file)
+                    airlines: dict = yaml.safe_load(arg_file)
                     airlines_df: pd.DataFrame = pd.json_normalize(airlines["airlines"])
 
+                    # airline_id | airline_name | airline_icao_unique_code | airline_country
                     df_list.append(airlines_df)
 
                 elif key == "AIRPORTS":
-                    airports = yaml.safe_load(arg_file)
+                    airports: dict = yaml.safe_load(arg_file)
                     airports_df: pd.DataFrame = pd.json_normalize(airports["airports"])
 
+                    # airport_id | airport_name | airport_city | airport_country | airport_icao_unique_code | airport_altitude
                     df_list.append(airports_df)
                 
                 elif key == "ROUTES":
-                    routes = yaml.safe_load(arg_file)
+                    routes: dict = yaml.safe_load(arg_file)
                     routes_df: pd.DataFrame = pd.json_normalize(routes["routes"])
 
-                    df_list.append(routes)
+                    # route_airline_id | route_from_airport_id | route_to_airport_id
+                    df_list.append(routes_df)
 
                 else:
                     continue
@@ -84,35 +86,54 @@ def create_arg_dataframes(inputs: dict) -> list:
 """
 Function:   a
 """
-def question(inputs: dict) -> None:
+def question_select(inputs: dict, df_list) -> None:
      parameters: dict
      key: str = "QUESTION"
      if key in inputs:
-         print(f"Question: {inputs[key]}")
+        #  print(f"Question: {inputs[key]}")
 
          if inputs[key] == "q1":
-            # specify the important data fields for producing
-            # the output to the question.
+            # Q - What are the top 20 airlines that offer the greatest
+            # number of routes with destination country Canada?
+
+            # airlines: airline_id, airline_name
+            # airports: airport_id, airport_country
+            # routes: route_airline_id, route_to_airport_id
             parameters = {"field1" : "add", "field2" : "add2"}
 
          if inputs[key] == "q2":
-            # specify the important data fields for producing
-            # the output to the question.
+            # Q - What are the top 30 countries with least appearances as
+            # destination country on the routes data?
+
+            # airlines: NA
+            # airports: airport_id, airport_country
+            # routes: route_to_airport_id 
             parameters = {"field1" : "add", "field2" : "add2"}
 
          if inputs[key] == "q3":
-            # specify the important data fields for producing
-            # the output to the question.
+            # Q - What are the top 10 destination airports?
+
+            # airlines: NA
+            # airports: airport_id, airport_name, airport_city, airport_country, airport_icao_unique_code
+            # routes: route_to_airport_id
             parameters = {"field1" : "add", "field2" : "add2"}
 
          if inputs[key] == "q4":
-            # specify the important data fields for producing
-            # the output to the question.
+            # Q - What are the top 10 destination cities?
+
+            # airlines: NA
+            # airports: airport_id, airport_city, airport_country
+            # routes: route_to_airport_id
             parameters = {"field1" : "add", "field2" : "add2"}
 
          if inputs[key] == "q5":
-            # specify the important data fields for producing
-            # the output to the question.
+            # Q - What are the unique top 10 Canadian routes with the
+            # most difference between the destination altitude and the
+            # origin altitude? (if CYYJ-CYVR is included, CYVR-CYYJ should not)
+
+            # airlines: NA
+            # airports: airport_id, airport_icao_unique_code, airport_altitude
+            # routes: route_from_airport_id, route_to_airport_id
             parameters = {"field1" : "add", "field2" : "add2"}
         
 
@@ -126,7 +147,7 @@ def main():
 
     df_list: list = create_arg_dataframes(inputs_dict)
 
-    question(inputs_dict)
+    question_select(inputs_dict, df_list)
 
 
 if __name__ == '__main__':
